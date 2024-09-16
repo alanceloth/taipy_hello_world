22227f55e9c61e4ed0b9d572668e1315d8b874e5
Alan Lanceloth <alan.lanceloth@gmail.com>
2024-09-15 21:55:09 -0300

    update

--- generate_datasets/load_raw_to_postgres.py
+++ generate_datasets/load_raw_to_postgres.py
@@ -15,6 +15,8 @@ POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 # String de conexão para o PostgreSQL
 postgres_conn = f"postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOSTNAME}:{POSTGRES_PORT}/{POSTGRES_DBNAME}"
 
+postgres_db = POSTGRES_DBNAME
+
 # Caminho para as pastas de arquivos Parquet
 parquet_dir_cadastros = './datasets/raw_data/cadastros/'
 parquet_dir_pedidos = './datasets/raw_data/pedidos/'
@@ -28,18 +30,35 @@ con.execute("""
     LOAD postgres_scanner;
 """)
 
+# Conectar ao PostgreSQL usando o comando ATTACH
+con.execute(f"""
+    ATTACH 'dbname={POSTGRES_DBNAME} user={POSTGRES_USER} host={POSTGRES_HOSTNAME} port={POSTGRES_PORT} password={POSTGRES_PASSWORD}' AS postgres_db (TYPE POSTGRES, SCHEMA 'public');
+""")
+
+# Função para criar a tabela se ela não existir
+def create_table_if_not_exists(postgres_table, schema):
+    con.execute(f"""
+        CREATE TABLE IF NOT EXISTS postgres_db.public.{postgres_table} AS 
+        SELECT * FROM {schema} LIMIT 0;
+    """)
+
 # Função para carregar arquivos Parquet e transferir para PostgreSQL
 def load_parquet_to_postgres(parquet_dir, postgres_table):
     parquet_files = os.path.join(parquet_dir, '*.parquet')
     
-    # Carregar todos os arquivos Parquet do diretório para uma tabela temporária no DuckDB
-    con.execute(f"CREATE OR REPLACE TEMPORARY VIEW temp_view AS SELECT * FROM read_parquet('{parquet_files}');")
+    # Carregar os arquivos Parquet no DuckDB
+    query = f"CREATE OR REPLACE TEMPORARY VIEW temp_view AS SELECT * FROM read_parquet('{parquet_files}');"
+    con.execute(query)
     
-    # Inserir os dados da tabela temporária no PostgreSQL
+    # Criar a tabela no PostgreSQL, se não existir
+    create_table_if_not_exists(postgres_table, 'temp_view')
+    
+    # Inserir os dados diretamente no PostgreSQL
     con.execute(f"""
-        COPY (SELECT * FROM temp_view) TO '{postgres_conn}.{postgres_table}'
-        (FORMAT CSV, HEADER);
+        INSERT INTO postgres_db.public.{postgres_table}
+        SELECT * FROM temp_view;
     """)
+    
     print(f"Dados de {parquet_dir} carregados na tabela {postgres_table} no PostgreSQL")
 
 # Carregar os arquivos de cadastros e pedidos para PostgreSQL
